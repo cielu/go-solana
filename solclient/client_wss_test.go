5eaf863fd2ab7dddb2732c2a9e0015f4c74f0067
ciel <ciel@gmail.com>
2024-04-10 17:33:45 +0800

    sub account info

--- solclient/client_wss_test.go
+++ solclient/client_wss_test.go
@@ -2,8 +2,8 @@ package solclient
 
 import (
 	"context"
+	"fmt"
 	"github.com/cielu/go-solana/common"
-	"github.com/cielu/go-solana/core"
 	"github.com/cielu/go-solana/types"
 	"testing"
 )
@@ -16,9 +16,26 @@ func TestClient_AccountSubscribe(t *testing.T) {
 	)
 	account := common.Base58ToAddress("CM78CPUeXjn8o3yroDHxUtKsZZgoy4GPkPPXfouKNH12")
 	//
-	res, err := c.AccountSubscribe(ctx, accountNotify, account)
+	sub, err := c.AccountSubscribe(ctx, accountNotify, account)
 	if err != nil {
 		t.Error("AccountSubscribe Failed: %w", err)
 	}
-	core.BeautifyConsole("AccountSubscribe:", res)
+	// if error
+	if err != nil {
+		panic(fmt.Sprintf("EthSubscribe Failed: %s", err.Error()))
+	}
+
+	defer sub.Unsubscribe()
+	// fmt.Println("Start BotClient Pointer:", bot)
+	// handler the subscribed pending hash
+	for {
+		select {
+		case err = <-sub.Err():
+			panic(fmt.Sprintf("[SUBSCRIPTION] Fatal error: %s", err.Error()))
+		// Code block is executed when a new tx hash is piped to the channel
+		case accountInfo := <-accountNotify:
+			// analyse transaction from hash by querying the client
+			fmt.Println(accountInfo)
+		}
+	}
 }
